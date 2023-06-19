75d0b57bef04c385a7a786cddfda1cd07c40ed0c
brunocriacoes <br.rafael@outlook.com>
2023-06-19 13:16:45 -0300

    send mail

--- rolex.php
+++ rolex.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Rolex
  * Description: Interação com Rolex, este plugin cria template de páginas
- * Version: 1.0.0
+ * Version: 1.1.0
  * Requires at least: 5.2
  * Requires PHP: 7.2
  * Author: Regulars Witch
